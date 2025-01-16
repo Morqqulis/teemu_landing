a423378cfce4bcb2103869326607173f5236b581
D <morqqulis@gmail.com>
2025-01-16 20:09:25 +0400

    without container

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,60 +4,58 @@ import { images, items } from './constants'
 
 const Hero = () => {
 	return (
-		<div className={`pb-20 md:-mt-[7%]`}>
-			<div className="container">
-				<div className={`animate-fade-up mb-20 flex items-center justify-between`}>
-					{images.map((img) => (
-						<div key={img.id}>
-							<Image src={img.src} width={img.width} height={img.height} alt={'Icon'} />
-						</div>
-					))}
-				</div>
-
-				<div className={`mb-20 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-5`}>
-					{items.map((item, index) => (
-						<div
-							className={`flex flex-col gap-2.5 ${index % 2 === 0 ? 'animate-fade-right' : 'animate-fade-left'}`}
-							key={item.id}
-						>
-							<Image src={item.src} width={236} height={236} alt={'Icon'} />
-							<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{item.title}</h3>
-							<p className="text-sm leading-7">{item.text}</p>
-						</div>
-					))}
-				</div>
-
-				<div className="flex flex-col justify-around md:flex-row">
-					<div className="flex items-center justify-center">
-						<Image className="h-auto" src={`/bottle.png`} alt="Field Bottle" width={300} height={400} />
+		<div className={`pb-20 md:-mt-[7%] px-6`}>
+			<div className={`animate-fade-up mb-20 flex items-center justify-between`}>
+				{images.map((img) => (
+					<div key={img.id}>
+						<Image src={img.src} width={img.width} height={img.height} alt={'Icon'} />
 					</div>
+				))}
+			</div>
 
-					<div className="max-w-md p-8">
-						<h2 className="mb-2 grid text-3xl font-light text-black">
-							field bottle
-							<span>$29</span>
-						</h2>
-						<p className="mb-4 text-sm">ships from the u.s.</p>
+			<div className={`mb-20 grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-5`}>
+				{items.map((item, index) => (
+					<div
+						className={`flex flex-col gap-2.5 ${index % 2 === 0 ? 'animate-fade-right' : 'animate-fade-left'}`}
+						key={item.id}
+					>
+						<Image src={item.src} width={236} height={236} alt={'Icon'} />
+						<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{item.title}</h3>
+						<p className="text-sm leading-7">{item.text}</p>
+					</div>
+				))}
+			</div>
 
-						<button className="w-full border bg-black px-4 py-3 text-white duration-300 hover:border-black hover:bg-white hover:text-black">
-							add to cart
-						</button>
+			<div className="flex flex-col justify-around md:flex-row">
+				<div className="flex items-center justify-center">
+					<Image className="h-auto" src={`/bottle.png`} alt="Field Bottle" width={300} height={400} />
+				</div>
 
-						<div className="space-y-4 pt-4 text-gray-500">
-							<p className="text-sm">
-								white stainless steel bottle with screw top and &quot;teenage engineering field system&quot; print. can
-								keep your drink hot or cold for 10-12 hours.
-							</p>
+				<div className="max-w-md p-8">
+					<h2 className="mb-2 grid text-3xl font-light text-black">
+						field bottle
+						<span>$29</span>
+					</h2>
+					<p className="mb-4 text-sm">ships from the u.s.</p>
 
-							<ul className="text-sm">
-								<li>• powder-coated stainless steel</li>
-								<li>• dimensions: 260 x 70 mm | 10.2 x 2.8 in, 18 oz | 532 ml</li>
-							</ul>
+					<button className="w-full border bg-black px-4 py-3 text-white duration-300 hover:border-black hover:bg-white hover:text-black">
+						add to cart
+					</button>
 
-							<Link href="#" className="block text-sm text-blue-600 hover:underline">
-								explore
-							</Link>
-						</div>
+					<div className="space-y-4 pt-4 text-gray-500">
+						<p className="text-sm">
+							white stainless steel bottle with screw top and &quot;teenage engineering field system&quot; print. can
+							keep your drink hot or cold for 10-12 hours.
+						</p>
+
+						<ul className="text-sm">
+							<li>• powder-coated stainless steel</li>
+							<li>• dimensions: 260 x 70 mm | 10.2 x 2.8 in, 18 oz | 532 ml</li>
+						</ul>
+
+						<Link href="#" className="block text-sm text-blue-600 hover:underline">
+							explore
+						</Link>
 					</div>
 				</div>
 			</div>
